1a48a7085aa21c06deb8250c9b8f2a66ed10a85e
MarcosTulioSDLV <mtsotodelavega@gmail.com>
2025-02-27 16:21:40 -0500

    update IngredientRequestDto:amount to @Positive commit

--- src/main/java/com/springboot/recipe_management_system/dtos/IngredientRequestDto.java
+++ src/main/java/com/springboot/recipe_management_system/dtos/IngredientRequestDto.java
@@ -3,7 +3,7 @@ package com.springboot.recipe_management_system.dtos;
 import com.springboot.recipe_management_system.enums.IngredientUnitEnum;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Getter @Setter
 public class IngredientRequestDto {
 
-    @NotNull @PositiveOrZero
+    @NotNull @Positive
     private Double amount;
 
     @NotNull
