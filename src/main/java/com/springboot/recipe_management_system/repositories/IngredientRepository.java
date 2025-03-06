31aa284cb458d48194b6648abeb7e55628f823fb
MarcosTulioSDLV <mtsotodelavega@gmail.com>
2025-03-06 15:03:25 -0500

    updated 2 GetForSelf methos FOR Ingredient commit

--- src/main/java/com/springboot/recipe_management_system/repositories/IngredientRepository.java
+++ src/main/java/com/springboot/recipe_management_system/repositories/IngredientRepository.java
@@ -16,13 +16,11 @@ public interface IngredientRepository extends JpaRepository<Ingredient,UUID> {
 
     Page<Ingredient> findAllByRecipe_User(UserEntity currentLoggedUser, Pageable pageable);
 
+    /*@Query("SELECT i FROM Ingredient i JOIN i.recipe r JOIN r.user u WHERE u=:currentLoggedUser")
+    Page<Ingredient> findAllByRecipe_UserCustom(UserEntity currentLoggedUser, Pageable pageable);*/
 
-    //TO DO: TESTING
-    @Query("SELECT i FROM Ingredient i JOIN i.recipe r JOIN r.user u WHERE u=:currentLoggedUSer")
-    Page<Ingredient> findAllByRecipe_UserCustom(UserEntity currentLoggedUser, Pageable pageable);
-
-    @Query(value="SELECT i.* FROM TB_INGREDIENT i INNER JOIN TB_RECIPE r ON i.recipe_id=r.id\n" +
+    /*@Query(value="SELECT i.* FROM TB_INGREDIENT i INNER JOIN TB_RECIPE r ON i.recipe_id=r.id\n" +
             " INNER JOIN TB_USER u ON r.user_id=u.id WHERE u.id=:userId",nativeQuery = true)
-    Page<Ingredient> findAllByUserIdCustomQuery(UUID userId, Pageable pageable);
+    Page<Ingredient> findAllByUserIdCustomQuery(UUID userId, Pageable pageable);*/
 
 }
