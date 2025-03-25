f8c397c3f90ee9682f29c48fe4c59a97d950b032
MarcosTulioSDLV <mtsotodelavega@gmail.com>
2025-03-25 12:12:33 -0500

    cleaned IngredientController commit

--- src/main/java/com/springboot/recipe_management_system/config/AppConfig.java
+++ src/main/java/com/springboot/recipe_management_system/config/AppConfig.java
@@ -1,10 +1,8 @@
 package com.springboot.recipe_management_system.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.ui.Model;
 
 @Configuration
 public class AppConfig {
